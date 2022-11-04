19dce441399ac66326839030cdd3315e6085ee6e
unknown <iamtheemailer@gmail.com>
2022-11-04 06:40:37 +0530

    check

---
+++ script.js
@@ -0,0 +1,12 @@
+function check() {
+    if (document.getElementById('check').checked) {
+        alert("checked");
+    } else {
+        alert("Not checked.");
+    }
+
+}
+
+function submit() {
+    alert("You are starting the stream!! Hurry happy to go")
+}
